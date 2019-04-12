e4e82104fd6f0d5daf72bb435b516c2a0f8fea4e
Rafał <Rafa@wp.pl>
2019-04-12 17:38:10 +0200

    Updates

---
+++ precache-manifest.516bd96549be0706036ca3c4e65b4701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91f38cf92fe368e1227f",
+    "url": "/Pogoda/static/js/runtime~main.b326004e.js"
+  },
+  {
+    "revision": "53d0e7d6e6bd12ddcca6",
+    "url": "/Pogoda/static/js/main.9ada5207.chunk.js"
+  },
+  {
+    "revision": "ee5e599cf10cc805d82c",
+    "url": "/Pogoda/static/js/2.695353e9.chunk.js"
+  },
+  {
+    "revision": "53d0e7d6e6bd12ddcca6",
+    "url": "/Pogoda/static/css/main.0813b381.chunk.css"
+  },
+  {
+    "revision": "52e1e784534ec169877c32011efa1a16",
+    "url": "/Pogoda/index.html"
+  }
+];
